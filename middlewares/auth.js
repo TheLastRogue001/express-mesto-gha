cda62245d7c0666f5678aab026eb45b00d013d6d
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-10-24 11:00:58 +0300

    fix: auth success

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,11 @@
 /* eslint-disable max-len */
 /* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('unauthorized');
 const {
-  JWT_SECRET, NODE_ENV, HTTP_STATUS_DENIED,
+  JWT_SECRET, NODE_ENV,
 } = require('../consts/consts');
 
-const handleAuthError = (res) => res.status(HTTP_STATUS_DENIED).send({ message: 'Необходима авторизация' });
 
 module.exports = (req, res, next) => {
   let payload;
@@ -15,15 +15,14 @@ module.exports = (req, res, next) => {
 
     if ((authorization && authorization.startsWith('Bearer ')) || (cookies && cookies.jwt)) {
       const token = authorization ? authorization.replace('Bearer ', '') : cookies.jwt;
-      // if (!token) return handleAuthError(res);
       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
-      res.clearCookie('jwt');
       req.user = payload;
+      res.clearCookie('jwt');
       next();
     } else {
-      return handleAuthError(res);
+      next(new UnauthorizedError('Неверные авторизационные данные'));
     }
   } catch (error) {
-    return handleAuthError(res);
+    next(new UnauthorizedError('Неверные авторизационные данные'));
   }
 };
