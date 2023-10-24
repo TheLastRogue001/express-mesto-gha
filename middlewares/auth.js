46f8ceae0e8ac6b7fa0805e4c9ccbe26ce36a30b
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-10-24 11:45:50 +0300

    fix: add remove jwt

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -17,6 +17,7 @@ module.exports = (req, res, next) => {
       const token = authorization ? authorization.replace('Bearer ', '') : cookies.jwt;
       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
       req.user = payload;
+      res.clearCookie('jwt');
       next();
     } else {
       next(handleAuthError(res));
