17bf90e6131d4a6dda0a5050ed2bcf8c6f80eb0f
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-10-24 16:25:30 +0300

    fix: add token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ const { JWT_SECRET, NODE_ENV } = require('../consts/consts');
 module.exports = (req, res, next) => {
   let payload;
   try {
-    const { authorization } = req.headers;
+    const { cookies } = req;
 
-    if (authorization && authorization.startsWith('Bearer ')) {
-      const token = authorization.replace('Bearer ', '');
+    if (cookies && cookies.jwt) {
+      const token = cookies.jwt;
       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
       req.user = payload;
       next();
